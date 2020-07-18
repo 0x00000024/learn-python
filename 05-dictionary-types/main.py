723f2d3be1229606dd4ee353a45c76cfad39e866
Ethan Yu <sohbmpethan@gmail.com>
2020-07-18 23:31:07 +0900

    05-dictionary-types

---
+++ 05-dictionary-types/main.py
@@ -0,0 +1,8 @@
+monday_temperatures = [9.1, 8.8, 7.5]
+student_grades = {"Marry": 9.1, "Sim": 8.8, "John": 7.5 }
+
+mysum = sum(student_grades.values())
+length = len(student_grades)
+mean = mysum / length
+
+print(mean)

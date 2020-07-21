3cca128df14d1e878e2e76db4331f9a94b5868d6
Ethan Yu <sohbmpethan@gmail.com>
2020-07-21 16:52:18 +0900

    01-Lists-Operations

--- 04-Basics-Data-Types-Operations/01-Lists-Operations/main.py
+++ 04-Basics-Data-Types-Operations/01-Lists-Operations/main.py
@@ -1,4 +1,10 @@
+tmp = 0
+
 monday_temperatures = [9.1, 8.8, 7.5]
 monday_temperatures.append(8.1)
+# monday_temperatures.clear()
+# tmp = monday_temperatures.index(8.8)
+monday_temperatures.remove(monday_temperatures[-1])
 
-print(monday_temperatures)
+tmp = monday_temperatures
+print(tmp)

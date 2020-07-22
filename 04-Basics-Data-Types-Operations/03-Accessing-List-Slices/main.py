e9d5ff2376b502e373a1f0e7a3bdea75e0833f17
Ethan Yu <sohbmpethan@gmail.com>
2020-07-22 16:54:24 +0900

    03-Accessing-List-Slices

---
+++ 04-Basics-Data-Types-Operations/03-Accessing-List-Slices/main.py
@@ -0,0 +1,10 @@
+tmp = 0
+
+monday_temperatures = [9.1, 8.8, 7.5, 6.6, 9.9]
+tmp = len(monday_temperatures)
+tmp = monday_temperatures[3]
+tmp = monday_temperatures[1:4]
+tmp = monday_temperatures[0:2]
+tmp = monday_temperatures[3:]
+
+print(tmp)

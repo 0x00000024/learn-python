7f06ec41837db9aae816382f40b1ecf5dd62bf99
Ethan Yu <sohbmpethan@gmail.com>
2020-07-22 17:02:17 +0900

    04-Accessing-List-Slices-Negative-Indexes

---
+++ 04-Basics-Data-Types-Operations/04-Accessing-List-Slices-Negative-Indexes/main.py
@@ -0,0 +1,10 @@
+tmp = 0
+
+monday_temperatures = [9.1, 8.8, 7.5, 6.6, 9.9]
+tmp = monday_temperatures[-1]
+tmp = monday_temperatures[-5]
+tmp = monday_temperatures[-2:]
+tmp = monday_temperatures[-4:-2]
+tmp = monday_temperatures[-5:-2]
+
+print(tmp)

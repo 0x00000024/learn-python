cc43feadaede07f1823cc5f1191bfd34ecac72e2
Ethan Yu <sohbmpethan@gmail.com>
2020-08-01 12:06:01 +0900

    02-Looping-Through-Dictionary

--- 07-Basics-Loops/02-Looping-Through-Dictionary/main.py
+++ 07-Basics-Loops/02-Looping-Through-Dictionary/main.py
@@ -1,5 +1,6 @@
 student_grades = {"Marry": 9.1, "Sim": 8.8, "John": 7.5}
 
 # for grades in student_grades.keys():
-for grades in student_grades.values():
-    print(grades)
+# for grades in student_grades.values():
+for k, v in student_grades.items():
+    print(k, v)
